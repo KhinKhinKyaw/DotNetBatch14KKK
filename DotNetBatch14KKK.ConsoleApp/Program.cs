57f8bd1fed192ebc690d743cc9ec5dcaa54fbcc5
Khin Khin Kyaw <Khin Khin Kyaw@LAPTOP-SA8AAJL9>
2024-11-16 10:42:47 +0630

    Add project files.

---
+++ DotNetBatch14KKK.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNetBatch14KKK.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
