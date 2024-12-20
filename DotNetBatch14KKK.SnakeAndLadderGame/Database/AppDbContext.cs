ccc1a202ef1dc95a44825d3cafa5932fb7be2dc4
Khin Khin Kyaw <khinkhinkyaw.dev@gmail.com>
2024-12-20 16:38:28 +0630

    Feat: SnakeAndLadderGame

---
+++ DotNetBatch14KKK.SnakeAndLadderGame/Database/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿
+using DotNetBatch14KKK.SnakeAndLadderGame.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DotNetBatch14KKK.SnakeAndLadderGame.Database;
+
+public class AppDbContext:DbContext
+{
+    public readonly SqlConnectionStringBuilder _sqlConnectionStringBuilder;
+    public AppDbContext()
+    {
+        _sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "DESKTOP-EJAVTIJ",
+            UserID = "sa",
+            Password = "sa@123",
+            InitialCatalog = "DotNetBatch14_UpdateSnakeLadder",
+            TrustServerCertificate = true
+        };
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_sqlConnectionStringBuilder.ConnectionString);
+        optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+    }
+
+    public DbSet<PlayerModel> Players { get; set; }
+    public DbSet<GameBoardModel> GameBoards { get; set; }
+    public DbSet<GameModel> Games { get; set; }
+    public DbSet<GameMoveModel> GameMoves { get; set; }
+}
---
+++ DotNetBatch14KKK.SnakeAndLadderGame/Features/SnakeAndLadderController.cs
@@ -0,0 +1,81 @@
+﻿using DotNetBatch14KKK.SnakeAndLadderGame.Models;
+using DotNetBatch14KKK.SnakeAndLadderGame.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace DotNetBatch14KKK.SnakeAndLadderGame.Features;
+[Route("api/[controller]")]
+[ApiController]
+public class SnakeAndLadderController : ControllerBase
+{
+    public readonly SnakeAndLadderService _Service;
+    public SnakeAndLadderController()
+    {
+        _Service = new SnakeAndLadderService();
+    }
+
+    [HttpPost("CreateGameBoard")]
+    public IActionResult CreateGameBoard(GameBoardModel RequestGameBoard)
+    {
+        try
+        {
+            var model = _Service.CreateGameBoard(RequestGameBoard);
+            if (!model.IsSuccess)
+            {
+                return BadRequest(model);
+            }
+            return Ok(model);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResponseModel()
+            {
+                Message = ex.ToString()
+            });
+        }
+    }
+
+    [HttpPost("CreateGame")]
+    public IActionResult CreateGame(List<PlayerModel> RequestPlayerModel)
+    {
+        try
+        {
+            var model = _Service.CreateGame(RequestPlayerModel);
+            if (!model.IsSuccess)
+            {
+                return BadRequest(model);
+            }
+            return Ok(model);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResponseModel()
+            {
+                Message = ex.ToString()
+            });
+        }
+    }
+
+    [HttpPost("PlayGame/{PlayerId}")]
+    public IActionResult PlayGame(int PlayerId)
+    {
+        try
+        {
+            var model = _Service.PlayGame(PlayerId);
+            if (!model.IsSuccess)
+            {
+                return BadRequest(model);
+            }
+            return Ok(model);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResponseModel()
+            {
+                Message = ex.ToString()
+            });
+        }
+
+    }
+}
