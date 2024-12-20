28ad892b9e01b5395d47472af76246245e16bb22
Khin Khin Kyaw <khinkhinkyaw.dev@gmail.com>
2024-12-20 16:39:39 +0630

    Feat: ConsoleAppSnakeAndLadder

---
+++ DotNetBatch14KKK.ConsoleAppSnakeAndLadder/GameModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetBatch14KKK.ConsoleAppSnakeAndLadder;
+
+[Table("tbl_game")]
+public class GameModel
+{
+    [Key]
+    public int? GameID { get; set; }
+    public string? GameStatus { get; set; }
+    public int CurrentPlayerID { get; set; }
+}
---
+++ DotNetBatch14KKK.ConsoleAppSnakeAndLadder/RefitClientService.cs
@@ -0,0 +1,54 @@
+﻿using DotNetBatch14KKK.ConsoleAppSnakeAndLadder;
+using Newtonsoft.Json;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DotNetBatch14KKK.ConsoleAppSnakeAndLadder
+{
+    public class RefitClientService
+    {
+        public readonly string endpoint = "https://localhost:7019";
+        public readonly ISnakeAndLadderApi _client;
+
+        public RefitClientService()
+        {
+            _client = RestService.For<ISnakeAndLadderApi>(endpoint);
+        }
+
+        public async Task<ResponseModel> CreateGameBoard(GameBoardModel RequestGameBoard)
+        {
+            var model = await _client.CreateGameBoard(RequestGameBoard);
+            return model;
+        }
+
+        public async Task<ResponseModel> CreateGame(List<PlayerModel> RequestPlayerModel)
+        {
+            var model = await _client.CreateGame(RequestPlayerModel);
+            return model;
+        }
+
+        public async Task<ResponseModel> PlayGame(int id)
+        {
+            var model = await _client.PlayGame(id);
+            return model;
+        }
+    }
+}
+public interface ISnakeAndLadderApi
+{
+    [Post("api/SnakeAndLadder/")]
+    Task<ResponseModel> CreateGameBoard(GameBoardModel RequestGameBoard);
+
+    [Post("/api/SnakeAndLadder/CreateGame")]
+    Task<ResponseModel> CreateGame(List<PlayerModel> RequestPlayerModel);
+
+    [Post("/api/SnakeAndLadder/PlayGame/{id}")]
+    Task<ResponseModel> PlayGame(int id);
+}
+
